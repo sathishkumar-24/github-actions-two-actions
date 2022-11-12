db5fdf673779ffe2e5538174b009e7d0c0fd3d8b
sathishkumar-24 <109523140+sathishkumar-24@users.noreply.github.com>
2022-11-12 10:12:29 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+                            Basic C Program.
+                        Addition of two numbers
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+*******************************************************************************/
+
+#include <stdio.h>
+int main()  {    
+
+    int number1, number2, sum;
+int main()
+{
+    printf("Hello World");
+      sum = number1 + number2;
+ printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
